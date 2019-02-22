bca97bd0537d7fb7423959595d619ff4d8728d60
Nikul619 <45201015+Nikul619@users.noreply.github.com>
2019-02-22 09:52:10 +0000

    Initial copy

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author npatel8
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String strVatialbe = "";
+		System.out.println("Hello World...");
+	}
+
+}
